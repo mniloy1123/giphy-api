604bb00c9286a42c23519d142f4df91de48798a2
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-19 23:23:32 -0400

    Configs

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/giphy-api",
   plugins: [react(), VitePluginNode()],
   define: {
     "process.env": process.env,
