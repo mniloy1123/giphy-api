1c87a5e0e438c0fa434243c3c4f687f6616899fd
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-19 22:01:32 -0400

    Fix bugs with duplicate gifs, move over the function for requestSearchGifs

--- src/components/GifCard.jsx
+++ src/components/GifCard.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
+import SearchField from "./SearchField";
 
 const GifCard = () => {
   const [gifs, setGifs] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     requestTrendingGifs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function requestTrendingGifs() {
@@ -14,18 +17,51 @@ const GifCard = () => {
     );
     const data = await res.json();
     const gifData = data.data;
-    setGifs(gifData);
+    setGifs(removeDuplicates(gifData));
   }
+
+  const handleSearch = async (searchTerm) => {
+    const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
+    const res = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}`
+    );
+    const data = await res.json();
+    const gifData = data.data;
+    setSearchResults(removeDuplicates(gifData));
+  };
+  //Fix error about duplicate gifs with same id on console
+  const removeDuplicates = (gifData) => {
+    const uniqueGifs = [];
+    const gifIds = new Set();
+    for (const gif of gifData) {
+      if (!gifIds.has(gif.id)) {
+        uniqueGifs.push(gif);
+        gifIds.add(gif.id);
+      }
+    }
+    return uniqueGifs;
+  };
 
   return (
     <div>
-      {gifs.map((gif) => (
-        <img
-          key={`gif-card-${gif.id}`}
-          src={gif.images.fixed_height.url}
-          alt="GIF"
-        />
-      ))}
+      <SearchField onSearch={handleSearch} />
+      <div>
+        {searchResults.length > 0
+          ? searchResults.map((gif) => (
+              <img
+                key={`search-${gif.id}`}
+                src={gif.images.fixed_height.url}
+                alt="GIF"
+              />
+            ))
+          : gifs.map((gif) => (
+              <img
+                key={`trending-${gif.id}`}
+                src={gif.images.fixed_height.url}
+                alt="GIF"
+              />
+            ))}
+      </div>
     </div>
   );
 };
