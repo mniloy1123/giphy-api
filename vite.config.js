b9659aa384f60bb789abc43c53d14f44169e7933
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-19 23:35:41 -0400

    More configs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
-import { VitePluginNode } from "vite-plugin-node";
 
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginNode()],
+  plugins: [react()],
   define: {
     "process.env": process.env,
   },
