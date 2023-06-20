89522175dd506c1cf091e9a5a7642e1f86cc2b44
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-19 23:20:10 -0400

    Add rating and language filter

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -4,6 +4,8 @@ import HomeIcon from "@mui/icons-material/Home";
 
 const SearchField = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [rating, setRating] = useState("");
+  const [language, setLanguage] = useState("");
 
   const handleHomeClick = () => {
     window.location.reload(); // Reload the page
@@ -12,7 +14,7 @@ const SearchField = ({ onSearch }) => {
   const handleSearch = () => {
     const trimmedSearchTerm = searchTerm.trim();
     if (trimmedSearchTerm) {
-      onSearch(trimmedSearchTerm);
+      onSearch(trimmedSearchTerm, rating, language);
     }
   };
 
@@ -34,6 +36,32 @@ const SearchField = ({ onSearch }) => {
       <button className="searchButton" onClick={handleSearch}>
         Search
       </button>
+      <select
+        className="filters"
+        value={rating}
+        onChange={(e) => setRating(e.target.value)}
+      >
+        <option value="">All Ratings</option>
+        <option value="g">G</option>
+        <option value="pg">PG</option>
+        <option value="pg-13">PG-13</option>
+        <option value="r">R</option>
+      </select>
+      <select
+        className="filters"
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
+      >
+        <option value="">All Languages</option>
+        <option value="en">English</option>
+        <option value="es">Spanish</option>
+        <option value="fr">French</option>
+        <option value="hi">Hindi</option>
+        <option value="bn">Bengali</option>
+        <option value="ru">Russian</option>
+        <option value="ja">Japanese</option>
+        <option value="ar">Arabic</option>
+      </select>
     </div>
   );
 };
